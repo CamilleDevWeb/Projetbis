596e3915db4f2d96736bc0461edc32b33ba11380
17010-75-06 <17010-75-06@exchange.ad.afpanet>
2022-02-14 16:37:56 +0100

    projet

--- Projetbis/Projetfilrouge_GreenGarden/projet_greengarden/script.js
+++ Projetbis/Projetfilrouge_GreenGarden/projet_greengarden/script.js
@@ -1,27 +1,4 @@
-/*function myFunction() {
-    var x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
-  }*/
 
-  function myFunction() {
-    var dots = document.getElementById("dots");
-    var moreText = document.getElementById("more");
-    var btnText = document.getElementById("myBtn");
-  
-    if (dots.style.display === "none") {
-      dots.style.display = "inline";
-      btnText.innerHTML = "Read more"; 
-      moreText.style.display = "none";
-    } else {
-      dots.style.display = "none";
-      btnText.innerHTML = "Read less"; 
-      moreText.style.display = "inline";
-    }
-  }
 
 
 // Compteur qui permet de savoir sur quelle slide nous sommes

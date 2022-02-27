44cdcfedef60dc1ccd7f477589f195d1da30fe78
CamilleDevWeb <95630059+CamilleDevWeb@users.noreply.github.com>
2022-02-27 21:11:59 +0100

    Projet

--- Projetbis/Projet_green_garden/script.js
+++ Projetbis/Projet_green_garden/script.js
@@ -1,6 +1,3 @@
-
-
-
 // Compteur qui permet de savoir sur quelle slide nous sommes
 let compteur = 0 
 let timer, elements, slides, slideWidth
@@ -80,4 +77,56 @@ function stopTimer(){
 function startTimer(){
     timer = setInterval(slideNext, 4000)
 }
+
+//menu//
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
+
+
+  function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
+
+
+  // formulaire
+  //créer un message d'alerte pour obliger l'utilisateur à avoir un minimum de 3 caractères
+let prenom = document.getElementById("firstname");
+let nom = document.getElementById("name");
+let datedenaissance = document.getElementById("date-of-birth");
+let email = document.getElementById("mail");
+let motdepasse = document.getElementById("pass");
+let confirmemotdepasse = document.getElementById("confirm_password");
+let boutton = document.getElementById("sendbutton")
+
+boutton.addEventListener("click", function (){
+    if (prenom.value.length <3){
+        alert("Attention : Votre prénom doit contenir 3 lettres au minimum !");
+    } else {
+    }
+    if (nom.value.length <3){
+        alert("Attention : Votre nom doit contenir 3 lettres au minimum !");
+    } else {
+    }
+    if (datedenaissance.value == ""){
+        alert("Attention : Veuillez saisir une date valide !");
+    } else {
+    }
+    if (email.value.indexOf("@") >1){
+    } else {
+        alert("Attention : Veuillez saisir un email valide !");
+    }
+    if (motdepasse != confirmemotdepasse){
+        alert("Attention : Les mots de passe ne correspondent pas !");
+    } else {
+    }
+})
 
